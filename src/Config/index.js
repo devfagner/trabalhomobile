6ef9b83777580480fbc733181d38e61962ed8786
devfagner <fagneranderle@gmail.com>
2022-07-06 20:34:22 -0300

    firebase e telas prontas

--- src/Config/index.js
+++ src/Config/index.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore'
 var firebaseConfig = {
-    apiKey: "AIzaSyCAVdYZg8kdetzYefWmkmuVLnbbGgA8yxg",
-    authDomain: "fema-mobile-grupo-3.firebaseapp.com",
-    projectId: "fema-mobile-grupo-3",
-    storageBucket: "fema-mobile-grupo-3.appspot.com",
-    messagingSenderId: "802953738691",
-    appId: "1:802953738691:web:2dcff3061f52d71b402576"
+    apiKey: "AIzaSyB4ImhxWUUk3iG-KS5UNALmnIrK99GWDwE",
+    authDomain: "trabalhomobile-68c5e.firebaseapp.com",
+    projectId: "trabalhomobile-68c5e",
+    storageBucket: "trabalhomobile-68c5e.appspot.com",
+    messagingSenderId: "57813391751",
+    appId: "1:57813391751:web:60e84e8d942249569ddb22"
   };
   // Initialize Firebase
 if(firebase.apps.length  === 0){
